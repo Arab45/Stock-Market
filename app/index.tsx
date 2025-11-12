18f7852b717777774f5a61319d79a2460ef94663
Arab45 <horlakunleolayiwola@gmail.com>
2025-11-12 04:26:29 -0800

    refactoring

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,7 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// app/portfolio.tsx
+
 import PortfolioScreen from "./screens/portfolioScreen";
 
-const Stack = createNativeStackNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Portfolio" component={PortfolioScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+export default function Portfolio() {
+  return <PortfolioScreen />;
 }
